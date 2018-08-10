657e3679f263e77e09a74975a0ed8473097766c6
James Sinkala <xinnks@gmail.com>
2018-08-10 04:48:01 +0300

    last update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,7 +61,7 @@
                                                 </a>
 
                                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                                    {{ csrf_field() }}
+                                                    @csrf
                                                 </form>
                                             </li>
                                         </ul>
